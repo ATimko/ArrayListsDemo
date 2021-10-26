e228d75811c91cd0cbfcdc8dea109e9415bad748
Ashley Timko <46455342+ATimko@users.noreply.github.com>
2021-10-26 09:15:19 -0700

    ArrayList Demo Code added

---
+++ ArrayListDemo.java
@@ -0,0 +1,75 @@
+/*
+   Ashley Timko
+   10/26/21
+   Description: ArrayList Demo
+*/
+import java.util.*;
+import java.in.*;
+
+public class ArrayListDemo{
+   public static void main(String[] args) throws FileNotFoundException{
+   //Declare scanner to read from file
+   //Scnr = ip
+   Scanner scnr = new Scanner(new File("words.txt"));
+   //Declare the ArrayList to store words
+   ArrayList<String> words = new ArrayList<String>();
+   
+   //Read from file and store elements in the ArrayList
+   //while(scnr.hasNext()) File is too large and will take time
+   for(int i = 0; i < 2000; i++){
+      words.add(scnr.next());
+   }
+   // Task1: Display the ArrayList
+   System.out.println(words);    //toString() of the ArrayList
+   
+   // Task 2: Display words in reverse order
+   displayReverse(words);
+   
+   // Task 3: Display words ending with "s" capitalized
+   displayPlurals(words);
+   
+   // Task 4: Remove words ending with "s"
+   removePlurals(words);
+   System.out.println(words);
+      
+   }
+   /*
+      Method: Display words in reverse order
+      Param: ArrayList<String>
+      Return: void
+   */
+   public static void displayReverse(ArrayList<String> list){
+      //Loop through the list
+      for(int i = list.size()-1; i >= 0; i--){
+         System.out.print(list.get(i) + " ");   
+      }
+      System.out.println();
+   }
+   /*
+      Method: Display words ending with "s" capitalized
+      Param: ArrayList<String>
+      Return: void
+   */
+   public static void displayPlurals(ArrayList<String> list){
+      //Loops through the list
+      for(String str:list){
+         if(str.endsWith("s"))
+            System.out.print(str.toUpperCase() + " ");
+      }
+      System.out.println();
+   }
+   /*
+      Method: Remove words ending with "s"
+      Param: ArrayList<String>
+      Return: void
+   */   
+   public static void removePlurals(ArrayList<String> list){
+      //Loop through the list
+      for(i = 0; i < list.size(); i++){
+         if(list.get(i).endsWith("s") || list.get(i).endsWith("S")){
+            list.remove(i);
+            i--;
+         }   
+      }
+   }
+}
